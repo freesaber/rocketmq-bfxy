c99f0b12707a4909aeaff0675414a4d550006388
freesaber <free.saber@qq.com>
2022-10-19 09:48:20 +0800

    3-1 生产者使用与管控台查询消息

--- rocketmq-api/src/main/java/com/bfxy/rocketmq/quickstart/Producer.java
+++ rocketmq-api/src/main/java/com/bfxy/rocketmq/quickstart/Producer.java
@@ -4,6 +4,7 @@ import com.bfxy.rocketmq.constants.Const;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
@@ -13,17 +14,20 @@ public class Producer {
         producer.setNamesrvAddr(Const.NAMESRV_ADDR);
         producer.start();
 
+        // 视频自动创建，如果报错，尝试先手动创建topic
         for (int i = 0; i < 5; i++) {
             // 创建消息
             Message message = new Message(
                     "test_quick_topic", // 主题
                     "TagA", // TAG
                     "key" + i, // KEY：用作唯一标识
-                    ("Hello RocketMQ").getBytes() // 内容
+                    ("Hello RocketMQ" + i).getBytes() // 内容
             );
             // 发送消息
-            producer.send(message);
+            SendResult send = producer.send(message);
+            System.out.println("消息发出" + send);
         }
 
+        producer.shutdown();
     }
 }
