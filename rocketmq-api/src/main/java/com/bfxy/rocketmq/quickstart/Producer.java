0b19f0deb0daa336f6b573e8284daf9d3038504d
freesaber <free.saber@qq.com>
2022-11-04 11:06:38 +0800

    1

--- rocketmq-api/src/main/java/com/bfxy/rocketmq/quickstart/Producer.java
+++ rocketmq-api/src/main/java/com/bfxy/rocketmq/quickstart/Producer.java
@@ -4,7 +4,9 @@ import com.bfxy.rocketmq.constants.Const;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
@@ -23,9 +25,29 @@ public class Producer {
                     "key" + i, // KEY：用作唯一标识
                     ("Hello RocketMQ" + i).getBytes() // 内容:字节类型
             );
-            // 发送消息
+
+            // 设置延迟发送
+            if(i == 1){
+                message.setDelayTimeLevel(3);
+            }
+
+            // 2.1 同步发送消息
             SendResult send = producer.send(message);
+            SendStatus status = send.getSendStatus();// 失败，需要做补偿
             System.out.println("消息发出" + send);
+
+            // 2.2 异步发送消息
+//            producer.send(message, new SendCallback() {
+//                @Override
+//                public void onSuccess(SendResult sendResult) {
+//                    System.out.println("msgId:" + sendResult.getMsgId() + ",status:"+sendResult.getSendStatus());
+//                }
+//
+//                @Override
+//                public void onException(Throwable throwable) {
+//                    System.out.println("发送失败");
+//                }
+//            });
         }
 
         producer.shutdown();
