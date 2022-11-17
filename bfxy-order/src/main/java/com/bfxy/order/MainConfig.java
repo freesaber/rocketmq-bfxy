21cc6d9f0f266e941f7b92d945a4f0c5163d14d5
freesaber <free.saber@qq.com>
2022-11-17 17:41:53 +0800

    运行

---
+++ bfxy-order/src/main/java/com/bfxy/order/MainConfig.java
@@ -0,0 +1,12 @@
+package com.bfxy.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.bfxy.order.*" })
+@MapperScan(basePackages = "com.bfxy.order.mapper")
+public class MainConfig {
+
+}
---
+++ bfxy-order/src/main/java/com/bfxy/order/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.bfxy.order.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        System.out.println("------------");
+        return "index";
+    }
+}
