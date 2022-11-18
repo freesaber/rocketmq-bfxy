2d59b65639fa8664b076a2e8692fe77adc47ca40
freesaber <free.saber@qq.com>
2022-11-18 14:41:06 +0800

    dubbo的环境搭建和使用

--- bfxy-order/src/main/java/com/bfxy/order/web/IndexController.java
+++ bfxy-order/src/main/java/com/bfxy/order/web/IndexController.java
@@ -1,14 +1,26 @@
 package com.bfxy.order.web;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.bfxy.store.service.api.HelloServiceApi;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @Reference(version = "1.0.0",
+            application = "${dubbo.application.id}",
+            interfaceName = "com.bfxy.store.service.HelloServiceApi",
+            check = false,
+            timeout = 1000,
+            retries = 0
+    )
+    private HelloServiceApi helloService;
+
     @RequestMapping("/index")
-    public String index(){
+    public String index(@RequestParam String name){
         System.out.println("------------");
-        return "index";
+        return helloService.sayHello(name);
     }
 }
